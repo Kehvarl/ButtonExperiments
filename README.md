55ece661f383c641b0632019e07a84164787b2cf
Kehvarl <Kehvarl@Kehvarl.com>
2025-01-05 19:03:46 -0700

    REvisiting Buttons

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
+An Action changes game state
+May require Resources to Trigger
+When Triggered:
+  May consume Resources
+  May create Resources
+  May create Agents
+  May change Agents
+  May create Buttons
+  May change Buttons
+
+A Button triggers an Action when Clicked
+
+An Agent triggers an Action on a Schedule
+
+
 Simpler Button definitions:
-generates:  Map of key:value pairs for the button's output.  eg: {loot:1, other:1}
-unlocks_after: What conditions must be met to unlock? eg: {loot:100, ship:1}
-costs: what it costs to click the button.
-on_click:   Symbol to call when clicking the button.  This must match a function name that we can call using "send"
+- generates:  Map of key:value pairs for the button's output.  eg: {loot:1, other:1}
+- unlocks_after: What conditions must be met to unlock? eg: {loot:100, ship:1}
+- costs: what it costs to click the button.
+- on_click:   Symbol to call when clicking the button.  This must match a function name that we can call using "send"
 
 Auto-click rethinking
-Instead of Auto-click being built into the buttons, we could have the main tick iterate through a list of auto-clicks and if the button is clickable, do the clicking.
-Auto-click definitions would need to include the button symbol, click frequency, and a current counter.
-eg: [:loot, 10, 0] Clicks the :loot button every 10 frames and the last click was 0 frames ago.
-There can be multiple auto-clickers for each button
-An auto-click must still honor the button's other restrictions.
-If an auto-click can't fire because the button is unclickable, it stays ready to fire as soon as the button can be clicked
+- Instead of Auto-click being built into the buttons, we could have the main tick iterate through a list of auto-clicks and if the button is clickable,
+  do the clicking.
+- Auto-click definitions would need to include the button symbol, click frequency, and a current counter.
+  - eg: [:loot, 10, 0] Clicks the :loot button every 10 frames and the last click was 0 frames ago.
+- There can be multiple auto-clickers for each button
+- An auto-click must still honor the button's other restrictions.
+- If an auto-click can't fire because the button is unclickable, it stays ready to fire as soon as the button can be clicked
 
 Destroy-on-0 buttons
-A countdown button that can be clicked any time, but is destroyed if it hits 0
+- A countdown button that can be clicked any time, but is destroyed if it hits 0
   Might be useful for a combat minigame.  Have one for each "cannon" on the ship or each "HP" or something.
-
-
--- app/main.rb
+++ app/main.rb
@@ -2,26 +2,7 @@ require 'app/button.rb'
 
 
 def init args
-    args.state.buttons = {
-        loot: DelayActivationButton.new({x:600, y:350, w:80, h:40,
-                                         unlocked:true,
-                                         value:"Loot", increment:1,
-                                         text:"Loot!", display:true}),
-        lootmore: CountDownButton.new({x:600, y:300, w:175, h:40,
-                                       unlocked:false, unlock_value:"Loot", unlock_cost:10,
-                                       value:"Loot", increment:10,
-                                       click_value:"Loot", click_cost:5,
-                                       text:"Loot More! (5)", display:true}),
-        autoloot: Button.new({x:700, y:350, w:150, h:40,
-                              unlocked:false, unlock_value:"Loot", unlock_cost:100,
-                              click_once:true,
-                              value:nil, increment:0,
-                              click_value:"Loot", click_cost:100,
-                              text:"AutoLoot (100)", display:false,
-                              color:{r: 240, g: 240, b: 240},
-                              change_other:{loot:{"auto_click=":true, "auto_click_rate=":1}}
-                    })
-    }
+    args.state.buttons = {}
     args.state.values = {}
 end
 
@@ -30,52 +11,4 @@ def tick args
     if args.state.tick_count == 0
         init args
     end
-
-    args.state.buttons.keys.each do |k|
-        b = args.state.buttons[k]
-        if b.unlocked == false and
-                args.state.values.key?(b.unlock_value) and
-                args.state.values[b.unlock_value] >= b.unlock_cost
-            b.unlocked = true
-        end
-        if b.unlocked
-            (value, increment) = b.tick(args)
-            if value
-                args.state.values[value] += increment
-            end
-            args.outputs.primitives << b.render
-        end
-    end
-
-    if args.inputs.mouse.click
-        args.state.buttons.keys.each do |k|
-            b = args.state.buttons[k]
-            if args.inputs.mouse.click.inside_rect?(b) and b.clickable
-                if b.click_cost > 0
-                    if args.state.values[b.click_value] >= b.click_cost
-                        args.state.values[b.click_value] -= b.click_cost
-                    else
-                        return
-                    end
-                end
-                (value, increment) = b.click(args)
-                if value
-                    args.state.values[value] += increment
-                end
-            end
-        end
-    end
-
-    args.state.buttons.keys.each_with_index do |k, i|
-        b = args.state.buttons[k]
-        if b.display
-            if not args.state.values.key?(b.value)
-                args.state.values[b.value] = 0
-            end
-        end
-    end
-    args.state.values.keys.each_with_index do |v, i|
-        args.outputs.primitives << {x: 0, y: 700 - (i * 15)  ,text: "#{v}: #{args.state.values[v]}", r: 0, g: 0, b: 0}.label!
-    end
 end
-
